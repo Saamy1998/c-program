cdeb14ae39d403dcacd8587ad7842528ca249e11
Saamy1998 <110920543+Saamy1998@users.noreply.github.com>
2022-09-05 21:32:58 +0530

    Create RemoveMid.c

---
+++ Training/RemoveMid.c
@@ -0,0 +1,11 @@
+/*Algorithm
+Step 1: START.
+Step 2: Declare four integer variables.
+Step 3: Get the number of elements in the array.
+Step 4: Get the elements.
+Step 5: Enter the position where you want to delete the element.
+Step 6: Cheak FOR condition.
+Step 7: If condition is true goto step 9 otherwise goto step 10.
+Step 8: a[i] = a[i+1] and goto step 7.
+Step 9: Print final array after inserting the value
+Step 10: STOP.*/
