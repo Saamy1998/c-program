0e55e18d92408f622f0e7d7d74f007cb94aa99e4
Saamy1998 <110920543+Saamy1998@users.noreply.github.com>
2022-09-26 15:28:41 +0530

    Create week.c

---
+++ Training/Switchcase/week.c
@@ -0,0 +1,78 @@
+/*Algorithm
+Step 1: START.
+Step 2: Initialize a integer.
+Step 3: Declare the week number from user.
+Step 4: If the user enters the number is from 1 to 7 then follows the below cases else follows the default case and exit the case.
+Step 5: Switch(operation)
+       a) case 1
+            Print as Monday
+            Break he case
+       b) case 2
+            Print as Tuesday
+            Break he case
+       c) case 3:
+            Print as Wednesday
+            Break he case
+       d) case 4
+            Print as Thursday
+            Break he case
+       e) case 5
+            Print as Friday
+            Break he case
+       f)case 6
+            Print as Saturday
+            Break he case
+       g) case 7
+            Prints as Sunday
+            Break he case
+       h) default
+            printf("Invalid input! Please enter week number between 1-7.")
+Step 6: STOP.*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int week;
+    printf("Enter week number(1-7): ");                 // Input week number from user
+    scanf("%d", &week);
+    switch(week)                                        // Prints the day of week name 
+    {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid input! Please enter week number between 1-7.");
+    }
+
+    return 0;
+}
+
+/*OUTPUT
+~/Switchcase/ $ gcc -o week week.c
+~/Switchcase/ $ ./week
+Enter week number(1-7): 5
+Friday~/Switchcase/ $ ./week
+Enter week number(1-7): 2
+Tuesday~/Switchcase/ $ ./week
+Enter week number(1-7): 8
+Invalid input! Please enter week number between 1-7*/
